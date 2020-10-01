ded7f758c182f819b131967b72c81e28970609f7
abhi-node <abhibalagurusamy@gmail.com>
2020-09-30 21:48:13 -0400

    added one command

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const config = require('./config.json')
+const command = require('./command')
 
 client.on('ready', () => {
     console.log("The client is ready")
+})
+
+command(client, 'hi', message => {
+    message.channel.send(`Hi ${message.author}`)
 })
 
 client.login(config.token)
