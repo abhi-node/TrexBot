03f0b7bf97849bdb0fa79302e83e2af39ca6ef0d
abhi-node <abhibalagurusamy@gmail.com>
2020-09-30 22:00:47 -0400

    added abouttrex and wassup as commands

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ const command = require('./command')
 client.on('ready', () => {
     console.log("The client is ready")
 
-    command(client, 'hi', message => {
+    command(client, ['hi', 'wassup'], message => {
         message.channel.send(`Hi ${message.author}`)
+    })
+    command(client, 'abouttrex', message => {
+        message.channel.send('Trex is a 15 year old streamer trying to make it big. He enjoys playing Rainbow Six Siege, Rocket League and many other games. He loves interacting with his followers and subs and likes to keep a friendly community that is open to everyone')
     })
 })
 
