33afba9da030b9756048e1acd6b7a87f3b78c702
abhi-node <abhibalagurusamy@gmail.com>
2020-10-01 09:21:24 -0400

    added mood command and random library

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 const config = require('./config.json')
 const command = require('./command')
+const random = require('random')
 
 client.on('ready', () => {
     console.log("The client is ready")
@@ -12,6 +13,16 @@ client.on('ready', () => {
     command(client, 'abouttrex', message => {
         message.channel.send('Trex is a 15 year old streamer trying to make it big. He enjoys playing Rainbow Six Siege, Rocket League and many other games. He loves interacting with his followers and subs and likes to keep a friendly community that is open to everyone')
     })
+    command(client, 'mood', message => {
+        if (random.float(min=0,max=1) > 0.45) {
+            message.channel.send('I am feeling happy today! :smiley:')
+        } else if (random.float > 0.3) {
+            message.channel.send('I am feeling ok :neutral_face:')
+        } else {
+            message.channel.send('I am feeling sad :frowning2:')
+        }
+    })
+
 })
 
 
