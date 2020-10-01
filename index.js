276d4399a7e89e82d9f145c52d5b81536dc1e5a3
abhi-node <abhibalagurusamy@gmail.com>
2020-10-01 09:31:02 -0400

    fixed bug

--- index.js
+++ index.js
@@ -14,9 +14,10 @@ client.on('ready', () => {
         message.channel.send('Trex is a 15 year old streamer trying to make it big. He enjoys playing Rainbow Six Siege, Rocket League and many other games. He loves interacting with his followers and subs and likes to keep a friendly community that is open to everyone')
     })
     command(client, 'mood', message => {
-        if (random.float(min=0,max=1) > 0.45) {
+        const randomFloat = random.float(min=0,max=1)
+        if (randomFloat > 0.45) {
             message.channel.send('I am feeling happy today! :smiley:')
-        } else if (random.float > 0.2) {
+        } else if (randomFloat > 0.2) {
             message.channel.send('I am feeling ok :neutral_face:')
         } else {
             message.channel.send('I am feeling sad :frowning2:')
