0d5563455db7df8c056ed2edefea9ca18d7ba7d9
abhi-node <abhibalagurusamy@gmail.com>
2020-10-01 11:32:36 -0400

    added bf command for sigma

--- index.js
+++ index.js
@@ -40,10 +40,10 @@ client.on('ready', () => {
         }
     })
     command(client, ['bf','boyfriend'], message => {
-        if (random.float(min=0,max=1) > 0.25) {
-            message.channel.send(`${message.author} we aren't dating :rolling_eyes:`)
-        } else {
+        if (message.user.presence.data.activity.name === 'dating TrexBot' && message.user.presence.data.activity.type == 4) {
             message.channel.send(`${message.author} hi baby :smiling_face_with_3_hearts::smiling_face_with_3_hearts:`)
+        } else {
+            message.channel.send(`${message.author} we aren't dating :rolling_eyes:`)
         }
     })
 
